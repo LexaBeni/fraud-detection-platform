5ae73cee5ea600d25c072f95393130029376b108
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-08-24 16:49:37 +0300

    Updated feature engineeering

--- src/features/engineering.py
+++ src/features/engineering.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 
 base_features = ['TransactionDT', "TransactionAmt", "ProductCD", "P_emaildomain", "R_emaildomain"]
-extended_features = base_features + ["card2", "card4", "card6", "addr1", "addr2", "dist1", "dist2"]
-symbols = ['V', 'D', 'M', 'C', 'id', "card"]
+extended_features = base_features + ["card1", "card2", "card4", "card6", "addr1", "addr2", "dist1", "dist2"]
+symbols = ['V', 'D', 'M', 'C', 'id', "card", "addr", "dist"]
 def add_missing_features(df):
 
     df= df.copy()
@@ -28,7 +28,9 @@ def add_time_features(df):
 def create_engineered_features(df):
     result = df[extended_features].copy()
 
-    result = add_missing_features(df)
+    missing_features = add_missing_features(df)
+    missing_columns = ["missing_count", *[f"missing_{symbol}_count" for symbol in symbols],]
+    result = result.join(missing_features[missing_columns])
     result = add_time_features(result)
 
     return result
