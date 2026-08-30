21ae678df4aadba9d8f8d3fcb30930b5aa0ab4a3
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-08-30 17:54:20 +0300

    Added missing '@' and included app's version

--- src/api/main.py
+++ src/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from src.api.routers.health import router as health_router
 
-app = FastAPI(title="Fraud Detection API")
+app = FastAPI(title="Fraud Detection API", version="1.0.0")
 
 app.include_router(health_router)
--- src/api/routers/health.py
+++ src/api/routers/health.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix="/health", tags=["Health"])
 
-router.get("/")
+@router.get("")
 def health_check():
     return {"status": "healthy"}
