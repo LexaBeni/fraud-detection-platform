2faaac553245a863ced6271913ca9da5ba9413d3
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-08-31 10:36:36 +0300

    Created a test for prepare_all_features

---
+++ src/api/functions/test_featurs.py
@@ -0,0 +1,22 @@
+from src.api.functions.features import prepare_all_features
+from src.api.schemas.prediction import PredictionRequest
+
+def test_prepare_all_features():
+    payload = PredictionRequest(
+        TransactionDT=123456,
+        TransactionAmt=100.50,
+        ProductCD="W",
+        P_emaildomain="gmail.com",
+        card1=1234,
+        card4="visa",
+        card6="debit",
+    )
+
+    df = prepare_all_features(payload)
+
+    assert len(df) == 1
+    assert len(df.columns) == 48
+    assert "isFraud" not in df.columns
+    print(len(df.columns))
+
+test_prepare_all_features()
