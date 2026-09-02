80657e334a4b114ad5e6e8c216fd7099ddf935cf
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-02 11:41:07 +0300

    Created UserResponse schema

--- src/api/schemas/user.py
+++ src/api/schemas/user.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel, Field, EmailStr
+from datetime import datetime
 
 class UserCreate(BaseModel):
     email: EmailStr = Field(min_length=5, max_length=75, description="User email")
     password: str = Field(min_length=3, max_length=50, description="User password")
+
+class UserResponse(BaseModel):
+    id: int
+    email: str
+    role: str
+    created_at: datetime
