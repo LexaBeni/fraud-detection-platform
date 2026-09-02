36a61aacbbe067954728a7301071848d11d26c6b
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-02 11:29:45 +0300

    Implemented InvalidCredential exception

--- src/api/core/exceptions.py
+++ src/api/core/exceptions.py
@@ -12,5 +12,11 @@ class UserAlreadyExists(AppException):
         self.information = information
 
         super().__init__(status_code=409, error_code="USER_ALREADY_EXISTS" , message=f"User with {self.information} already exists", details=self.information)
-        
+
+class InvalidCredentials(AppException):
+
+    def __init__(self):
+
+        super().__init__(status_code=401, error_code="INVALID_CREDENTIALS", message="Invalid email or password")
+
     
