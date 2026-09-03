e7bc73d13b86f40ac25a1acb768348790f2579f3
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-03 14:25:58 +0300

    Created get single prediction endpoint

--- src/api/routers/prediction.py
+++ src/api/routers/prediction.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
-from src.api.schemas.prediction import PredictionRequest, PredictionResponse
+from src.api.schemas.prediction import PredictionRequest, PredictionResponse, PredictionHistoryResponse
 from src.dependencies.model import get_model
 from src.dependencies.database import get_db
 from src.api.services.prediction_service import PredictionService
@@ -17,4 +17,11 @@ def predict(df:PredictionRequest, model = Depends(get_model), db= Depends(get_db
         logger.exception(e)
 
         raise HTTPException(status_code=500, detail="Prediction failed.")
+
+@router.get("/history/{id}", response_model=PredictionHistoryResponse)
+def get_prediction(id: int, db = Depends(get_db), model = Depends(get_model), user= Depends(get_current_user)):
+    service = PredictionService(db=db, model=model)
+
+    return service.get_prediction(prediction_id=id, user=user)
+
     
