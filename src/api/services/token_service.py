797a5b44d4f76042e2fd97cd96e40f566bc23023
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-03 11:00:33 +0300

    Created token service

---
+++ src/api/services/token_service.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta, timezone
+from jose import jwt
+from src.api.core.settings import settings
+
+class TokenService():
+
+    @staticmethod
+    def create_token(data: dict, exp_delta: timedelta):
+        to_encode = data.copy()
+        expire = datetime.now(timezone.utc) + exp_delta
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, settings.jwt_secret_key, settings.jwt_algorithm)
+
+        return encoded_jwt
+
+    @staticmethod
+    def create_access_token(user):
+        return TokenService.create_token({"user_id": str(user.id), "type": "access"}, exp_delta = timedelta(minutes=settings.access_token_expires_minutes))
+
+    @staticmethod
+    def create_refresh_token(user):
+        return TokenService.create_token({"user_id": str(user.id), "type": "refresh"}, exp_delta=timedelta(days=settings.refresh_token_expires_days))
+    
