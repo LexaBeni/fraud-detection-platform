be0a00ff37a15b9bfcf2f81279ca60aa669505c0
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-03 18:32:00 +0300

    Created refresh_token database model

---
+++ src/api/models/refresh_token.py
@@ -0,0 +1,35 @@
+from src.api.core.database import Base
+from datetime import datetime
+from sqlalchemy import Boolean, String, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from src.api.models.user import User
+
+class RefreshToken(Base):
+
+    __tablename__ = "refresh_tokens"
+
+    id: Mapped[str] = mapped_column(
+        Integer, primary_key=True, autoincrement=True
+    )
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"), nullable=False
+    )
+
+    token_hash: Mapped[str] = mapped_column(
+        String(128), nullable=False, unique=True
+    )
+
+    exprires_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False
+    )
+
+    revoked: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, default=datetime.utcnow
+    )
+
+    user: Mapped["User"] = relationship(back_populates="refresh_tokens")
