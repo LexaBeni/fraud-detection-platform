4e26b5b8c4d42de2e8efd4be9f10a5c17f2d5b3b
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-03 17:12:48 +0300

    Added jweerror

--- src/dependencies/auth.py
+++ src/dependencies/auth.py
@@ -4,17 +4,21 @@ from src.api.core.security import oauth2_scheme
 from sqlalchemy.orm import Session
 from fastapi import Depends, status, HTTPException
 from jose import jwt
-from jose.exceptions import ExpiredSignatureError
+from jose.exceptions import ExpiredSignatureError, JWEError
 from src.api.core.exceptions import InvalidCredentials
 
 def decode_token(token: str):
     try:
-        payload = jwt.decode(token=token, key=settings.jwt_secret_key, algorithms=settings.jwt_algorithm)
+        payload = jwt.decode(token=token, key=settings.jwt_secret_key, algorithms=[settings.jwt_algorithm])
         sub = payload.get("sub")
         if not sub:
             raise InvalidCredentials()
     except ExpiredSignatureError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token has expired")
+
+    except JWEError:
+        raise InvalidCredentials()
+    
     except Exception:
         raise InvalidCredentials()
 
