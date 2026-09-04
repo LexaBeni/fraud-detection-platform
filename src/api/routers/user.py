3ff8ea70cb7d4d0498593e02111a0a5c01468adc
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-04 22:09:18 +0300

    Created refresh endpoint; not complete yet

--- src/api/routers/user.py
+++ src/api/routers/user.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import Session
 from src.dependencies.database import get_db
 from src.api.services.user_service import UserService
 from src.api.schemas.user import UserCreate, UserResponse
-from src.api.schemas.auth import TokenResponse
+from src.api.schemas.auth import TokenResponse, RefreshTokenRequest
 from fastapi.security import OAuth2PasswordRequestForm
-from src.dependencies.auth import get_current_user
+from src.dependencies.auth import get_current_user, decode_refresh_token
 from src.api.models.user import User
 from src.api.services.token_service import TokenService
 from src.api.services.refresh_token_service import RefreshTokenService
@@ -41,4 +41,19 @@ def login(data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get
         access_token=access_token.token,
         refresh_token=refresh_token.token
     )
+
+@router.post("/refresh", response_model=TokenResponse)
+def refresh(data: RefreshTokenRequest, db: Session = Depends(get_db)):
+    refresh_token = data.refresh_token
+
+    payload = decode_refresh_token(refresh_token)
+
+    user_service = UserService(db)
+
+    user_id = payload['sub']
+
+    user = user_service.get_user_by_id(int(user_id))
+
+    new_access_token = TokenService.create_access_token(user)
+
 
