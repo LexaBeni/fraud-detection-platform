147ff3f3b806493a2ce8df57099a3fae53f56638
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-06 15:16:22 +0300

    Created user_login test and adjusted user_create

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -1,9 +1,30 @@
-def test_user_create(client):
+import pytest
+
+@pytest.fixture
+def user_create(client):
     data = {"email": "test@test.com", "password": "test"}
 
     res = client.post("/auth/register", json=data)
 
+    user = res.json()
+
     assert res.status_code == 201
-    assert "password" not in res.json()
-    assert res.json()['role'] == "user"
-    print(res.json())
+    assert "password" not in user
+    assert user['role'] == "user"
+
+    user['password'] = data['password']
+
+    return user
+
+def test_user_login(client, user_create):
+
+    res = client.post("/auth/login", data = {"username": user_create['email'], "password": user_create['password']})
+
+    tokens = res.json()
+
+    assert res.status_code == 200
+    assert "refresh_token" in tokens
+    assert "access_token" in tokens
+    assert tokens["token_type"] == "bearer"
+
+    return tokens
