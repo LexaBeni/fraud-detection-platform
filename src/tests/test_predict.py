1d0b22df3f9aeaf0a8bb3c971d64260a3d7f2d5b
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-06 16:19:48 +0300

    Created additional predict tests

--- src/tests/test_predict.py
+++ src/tests/test_predict.py
@@ -1,3 +1,5 @@
+import pytest
+
 def test_predict(client, auth_header):
     data = {
         "TransactionDT": 86400,
@@ -24,3 +26,37 @@ def test_predict(client, auth_header):
     result = res.json()
 
     assert result['prediction'] == "VALID"
+
+@pytest.mark.parametrize("field, value", [
+    ("TransactionDT", -1),
+    ("TransactionAmt", -10),
+    ("dist1", -1),
+    ("dist2", -1),
+    ("D1", -1),
+])
+def test_invalid_predict(client, auth_header, field, value):
+    data = {
+        "TransactionDT": 86400,
+        "TransactionAmt": 49.50,
+        "ProductCD": "W",
+        "dist1": 19.0,
+        "dist2": None,
+        "D1": 14.0,
+    }
+
+    data[field] = value
+
+    res = client.post("/predict", json = data, headers=auth_header)
+
+    assert res.status_code == 422
+
+def test_predict_without_auth(client):
+    data = {
+        "TransactionDT": 86400,
+        "TransactionAmt": 49.50,
+        "ProductCD": "W",
+    }
+
+    res = client.post("/predict", json=data)
+
+    assert res.status_code == 401
