04900834d7feddb74b262cf8600aa3a8684b954b
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-07 11:11:45 +0300

    Created test_get_prediction_no_auth test and fixed minor errors in the previous get tests

--- src/tests/test_predict.py
+++ src/tests/test_predict.py
@@ -66,11 +66,25 @@ def test_get_prediction(client, create_prediction, auth_header):
     assert data['created_at'] is not None
     assert data['threshold'] == 0.18
     assert data['prediction'] in ["FRAUD", "VALID"]
-    assert data['probability'] <= 0.9
+    assert data['probability'] <= 1
 
 def test_get_prediction_no_auth(client, create_prediction):
     id = create_prediction["id"]
 
-    res = client.get("predict/history/{id}")
+    res = client.get(f"/predict/history/{id}")
 
     assert res.status_code == 401
+
+def test_get_history(client, create_prediction, auth_header):
+    res = client.get("/predict/history", headers=auth_header)
+
+    id = create_prediction['id']
+
+    data = res.json()[0]
+
+    assert res.status_code == 200
+    assert data['id'] == id
+    assert data['created_at'] is not None
+    assert data['threshold'] == 0.18
+    assert data['prediction'] in ["FRAUD", "VALID"]
+    assert data['probability'] <= 0.9
