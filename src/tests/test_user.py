cdd34fd0863a26c271ba3d951c5f325d2635554a
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-07 20:41:54 +0300

    Created refresh_token tests

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -13,3 +13,51 @@ def test_admin_login(login_admin):
 
 def test_admin_auth(auth_admin):
     assert "Authorization" in auth_admin
+
+def test_refresh_token(client, user_login):
+    refresh_token = user_login["refresh_token"]
+
+    res = client.post("/auth/refresh", json={"refresh_token": refresh_token})
+
+    assert res.status_code == 200
+
+    data = res.json()
+
+    assert "access_token" in data
+    assert "refresh_token" in data
+    assert data["token_type"] == "bearer"
+
+def test_old_refresh_token(client, user_login):
+    refresh_token = user_login["refresh_token"]
+    
+    res = client.post("/auth/refresh", json={"refresh_token": refresh_token})
+
+    assert res.status_code == 200
+
+    data = res.json()
+
+    assert data["refresh_token"] != refresh_token
+
+    res = client.post("/auth/refresh", json={"refresh_token": refresh_token})
+
+    assert res.status_code == 401
+
+def test_random_refresh_token(client):
+    res = client.post("/auth/refresh", json={"refresh_token": "randon_token"})
+
+    assert res.status_code == 401
+    assert "refresh_token" not in res.json()
+
+def test_access_token_instead_od_refresh(client, user_login):
+    access_token = user_login['access_token']
+
+    res = client.post("/auth/refresh", json={"refresh_token": access_token})
+    
+    assert res.status_code == 401
+
+def test_no_refresh_token(client):
+    res = client.post("/auth/refresh")
+
+    assert res.status_code == 422
+    
+    
