a859b9615ffc7fd232b7e77062574e4fa67caf97
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-07 20:49:53 +0300

    Added user create and login tests

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -2,6 +2,34 @@ import pytest
 from src.roles import UserRole
 from src.api.core.settings import settings
 
+def user_login(client, user_create):
+
+    res = client.post("/auth/login", data = {"username": user_create['email'], "password": user_create['password']})
+
+    tokens = res.json()
+
+    assert res.status_code == 200
+    assert "refresh_token" in tokens
+    assert "access_token" in tokens
+    assert tokens["token_type"] == "bearer"
+
+    return tokens
+
+def user_create(client):
+    data = {"email": "test@test.com", "password": "test"}
+
+    res = client.post("/auth/register", json=data)
+
+    user = res.json()
+
+    assert res.status_code == 201
+    assert "password" not in user
+    assert user['role'] == "user"
+
+    user['password'] = data['password']
+
+    return user
+
 def test_admin_create(create_admin):
     assert create_admin.role == UserRole.ADMIN
     assert create_admin.email == settings.admin_email
