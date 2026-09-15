d1bc521611bb356bb5595a30c4fd43e53b310e26
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-15 19:21:07 +0200

    Added database_server_url variable

--- src/api/core/settings.py
+++ src/api/core/settings.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     access_token_expires_minutes: int = 1
     refresh_token_expires_days: int = 1
     test_database_url: str = ""
+    database_server_url: str = ""
 
     model_config = SettingsConfigDict(
         env_file=".env",
