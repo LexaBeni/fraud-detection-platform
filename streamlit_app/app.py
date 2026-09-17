85357ac0ca7d6877f6f4e8365f1d3065ad1dd707
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-17 18:30:41 +0200

    Implemented history and delete endpoints

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-from streamlit_app.api import login_user, predict, register_user
+from streamlit_app.api import (
+    delete_prediction,
+    get_history,
+    get_prediction,
+    login_user,
+    predict,
+    register_user,
+)
 
 st.set_page_config(page_title="Fraud Detection", layout="wide")
 
@@ -28,69 +35,91 @@ if not st.session_state["access_token"]:
 else:
     with st.sidebar:
         st.success("Logged in")
+        page = st.radio("Prediction Page", ["Prediction", "History"])
         if st.button("Log out"):
             st.session_state["access_token"] = None
 
-    st.title("Fraud Detection")
+    if page == "Prediction":
+        st.title("Fraud Detection")
 
-    st.write("Detect potentially fraudulent transactions using machine learning.")
+        st.write("Detect potentially fraudulent transactions using machine learning.")
 
-    st.divider()
+        st.divider()
 
-    with st.form("Prediction form"):
-        transaction_dt = st.number_input("TransactionDT", min_value=0)
-        transaction_amt = st.number_input("TransactionAmt", min_value=0, value=100)
-        product_cd = st.selectbox("ProductCD", options=["W", "C", "R", "H", "S"])
-        p_emaildomain = st.text_input("P_emaildomain")
-        r_emaildomain = st.text_input("R_emaildomain")
-        p_emaildomain = p_emaildomain or None
-        r_emaildomain = r_emaildomain or None
-        card4 = st.selectbox(
-            "card4",
-            options=["visa", "mastercard", "american express", "discover", None],
-        )
-        card6 = st.selectbox(
-            "card6", options=["credit", "debit", "debit or credit", "charge card", None]
-        )
+        with st.form("Prediction form"):
+            transaction_dt = st.number_input("TransactionDT", min_value=0)
+            transaction_amt = st.number_input("TransactionAmt", min_value=0, value=100)
+            product_cd = st.selectbox("ProductCD", options=["W", "C", "R", "H", "S"])
+            p_emaildomain = st.text_input("P_emaildomain")
+            r_emaildomain = st.text_input("R_emaildomain")
+            p_emaildomain = p_emaildomain or None
+            r_emaildomain = r_emaildomain or None
+            card4 = st.selectbox(
+                "card4",
+                options=["visa", "mastercard", "american express", "discover", None],
+            )
+            card6 = st.selectbox(
+                "card6",
+                options=["credit", "debit", "debit or credit", "charge card", None],
+            )
 
-        def nullable_number_input(label, min_value=0):
-            is_missing = st.checkbox(f"{label} is missing", value=True)
+            def nullable_number_input(label, min_value=0):
+                is_missing = st.checkbox(f"{label} is missing", value=True)
 
-            if is_missing:
-                return None
+                if is_missing:
+                    return None
 
-            return st.number_input(label, min_value=min_value)
+                return st.number_input(label, min_value=min_value)
 
-        card1 = nullable_number_input("card1", min_value=1000)
-        card2 = nullable_number_input("card2", min_value=100)
-        card5 = nullable_number_input("card5", min_value=100)
+            card1 = nullable_number_input("card1", min_value=1000)
+            card2 = nullable_number_input("card2", min_value=100)
+            card5 = nullable_number_input("card5", min_value=100)
 
-        addr1 = nullable_number_input("addr1")
-        addr2 = nullable_number_input("addr2")
-        dist1 = nullable_number_input("dist1")
-        dist2 = nullable_number_input("dist2")
-        d1 = nullable_number_input("D1")
+            addr1 = nullable_number_input("addr1")
+            addr2 = nullable_number_input("addr2")
+            dist1 = nullable_number_input("dist1")
+            dist2 = nullable_number_input("dist2")
+            d1 = nullable_number_input("D1")
 
-        button = st.form_submit_button("Submit")
-        if button:
-            transaction = {
-                "TransactionDT": transaction_dt,
-                "TransactionAmt": transaction_amt,
-                "ProductCD": product_cd,
-                "P_emaildomain": p_emaildomain,
-                "R_emaildomain": r_emaildomain,
-                "card1": card1,
-                "card2": card2,
-                "card4": card4,
-                "card5": card5,
-                "card6": card6,
-                "addr1": addr1,
-                "addr2": addr2,
-                "dist1": dist1,
-                "dist2": dist2,
-                "D1": d1,
-            }
+            button = st.form_submit_button("Submit")
+            if button:
+                transaction = {
+                    "TransactionDT": transaction_dt,
+                    "TransactionAmt": transaction_amt,
+                    "ProductCD": product_cd,
+                    "P_emaildomain": p_emaildomain,
+                    "R_emaildomain": r_emaildomain,
+                    "card1": card1,
+                    "card2": card2,
+                    "card4": card4,
+                    "card5": card5,
+                    "card6": card6,
+                    "addr1": addr1,
+                    "addr2": addr2,
+                    "dist1": dist1,
+                    "dist2": dist2,
+                    "D1": d1,
+                }
 
-            result = predict(transaction)
+                result = predict(transaction)
 
-            st.json(result)
+                st.json(result)
+
+    if page == "History":
+        st.title("Prediction History")
+        st.write("Select or delete your prediction.")
+        with st.form("Specific History Form"):
+            st.subheader("Select your specific prediction.")
+            id = st.number_input("Prediction ID", min_value=0, step=1)
+            if st.form_submit_button("Get prediction"):
+                result = get_prediction(int(id))
+
+        with st.form("History Form"):
+            st.subheader("See all your predictions.")
+            if st.form_submit_button("Get all predictions"):
+                result = get_history()
+        with st.form("Delete Form"):
+            st.subheader("Delete your prediction")
+            id = st.number_input("Prediction id", min_value=0, step=1)
+            if st.form_submit_button(f"Delete prediction with id {id}"):
+                result = delete_prediction(id)
