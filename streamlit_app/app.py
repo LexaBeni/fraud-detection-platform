bc3d350a630c27c7a817d28e63c2a7dc032306f9
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-18 14:00:38 +0200

    Added expanders

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -53,21 +53,6 @@ else:
         st.divider()
 
         with st.form("Prediction form"):
-            transaction_dt = st.number_input("TransactionDT", min_value=0)
-            transaction_amt = st.number_input("TransactionAmt", min_value=0, value=100)
-            product_cd = st.selectbox("ProductCD", options=["W", "C", "R", "H", "S"])
-            p_emaildomain = st.text_input("P_emaildomain")
-            r_emaildomain = st.text_input("R_emaildomain")
-            p_emaildomain = p_emaildomain or None
-            r_emaildomain = r_emaildomain or None
-            card4 = st.selectbox(
-                "card4",
-                options=["visa", "mastercard", "american express", "discover", None],
-            )
-            card6 = st.selectbox(
-                "card6",
-                options=["credit", "debit", "debit or credit", "charge card", None],
-            )
 
             def nullable_number_input(label, min_value=0):
                 is_missing = st.checkbox(f"{label} is missing", value=True)
@@ -77,15 +62,48 @@ else:
 
                 return st.number_input(label, min_value=min_value)
 
-            card1 = nullable_number_input("card1", min_value=1000)
-            card2 = nullable_number_input("card2", min_value=100)
-            card5 = nullable_number_input("card5", min_value=100)
+            with st.expander("Transaction", expanded=True):
+                transaction_dt = st.number_input("TransactionDT", min_value=0)
+                transaction_amt = st.number_input(
+                    "TransactionAmt", min_value=0, value=100
+                )
+                product_cd = st.selectbox(
+                    "ProductCD", options=["W", "C", "R", "H", "S"]
+                )
+            with st.expander("Email"):
+                p_emaildomain = st.text_input("P_emaildomain")
+                r_emaildomain = st.text_input("R_emaildomain")
+            p_emaildomain = p_emaildomain or None
+            r_emaildomain = r_emaildomain or None
+            with st.expander("Cards"):
+                card4 = st.selectbox(
+                    "card4",
+                    options=[
+                        "visa",
+                        "mastercard",
+                        "american express",
+                        "discover",
+                        None,
+                    ],
+                )
+                card6 = st.selectbox(
+                    "card6",
+                    options=["credit", "debit", "debit or credit", "charge card", None],
+                )
 
-            addr1 = nullable_number_input("addr1")
-            addr2 = nullable_number_input("addr2")
-            dist1 = nullable_number_input("dist1")
-            dist2 = nullable_number_input("dist2")
-            d1 = nullable_number_input("D1")
+                card1 = nullable_number_input("card1", min_value=1000)
+                card2 = nullable_number_input("card2", min_value=100)
+                card5 = nullable_number_input("card5", min_value=100)
+
+            with st.expander("Address Information"):
+                addr1 = nullable_number_input("addr1")
+                addr2 = nullable_number_input("addr2")
+
+            with st.expander("Distance"):
+                dist1 = nullable_number_input("dist1")
+                dist2 = nullable_number_input("dist2")
+            with st.expander("Other"):
+                d1 = nullable_number_input("D1")
 
             button = st.form_submit_button("Submit")
             if button:
