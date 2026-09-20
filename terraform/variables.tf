aa0cc183cc31a67127136a6c9f3a61daca7c3aad
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-20 19:28:06 +0200

    Configured variables for streamlit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,8 @@ variable "fastapi_image"{
   type = string
   default = "773658094755.dkr.ecr.eu-central-1.amazonaws.com/fastapi-fraud-detection:e3c5d17b19c10f0e4ea6955b84be786c7fe5c51a"
 }
+
+variable "streamlit_image"{
+  type = string
+  default = "773658094755.dkr.ecr.eu-central-1.amazonaws.com/fraud-detection-streamlit:8602bad6457b7c9e12e95a04caba8af2505c7370"
+}
