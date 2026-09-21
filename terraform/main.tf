916b02ee3b63b8b107732df5ecfb4a057f4ec3ab
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-21 22:08:55 +0200

    Prevented ecr repositories from being destroyed

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,15 @@
 resource "aws_ecr_repository" "streamlit" {
   name = "fraud-detection-streamlit"
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_ecr_repository" "fastapi" {
   name = "fastapi-fraud-detection"
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_ecs_cluster" "main" {
