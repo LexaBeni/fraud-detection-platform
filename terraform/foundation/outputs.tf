86647c4731e5cccc04631057214268c33836d116
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-22 21:18:43 +0200

    Applied terraform fmt

--- terraform/foundation/outputs.tf
+++ terraform/foundation/outputs.tf
@@ -2,6 +2,6 @@ output "fastapi_ecr_repository_url" {
   value = aws_ecr_repository.fastapi.repository_url
 }
 
-output "streamlit_ecr_repository_url"{
-    value = aws_ecr_repository.streamlit.repository_url
+output "streamlit_ecr_repository_url" {
+  value = aws_ecr_repository.streamlit.repository_url
 }
