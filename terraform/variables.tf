b46aa7fd6e1a61b5efc21e526abaa58561967fd6
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-22 13:41:15 +0200

    Added infrastructure_enable variable; stable configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,3 +12,9 @@ variable "streamlit_image" {
   type    = string
   default = "773658094755.dkr.ecr.eu-central-1.amazonaws.com/fraud-detection-streamlit:8602bad6457b7c9e12e95a04caba8af2505c7370"
 }
+
+variable "infrastructure_enable" {
+  description = "Whether application infrastructure should be created"
+  type = bool
+  default = true
+}
