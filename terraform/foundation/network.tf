fe2432f0b63f17a953c7cb4f49c42f1eee9c5054
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-23 21:06:37 +0200

    Moved aws_security_group.fastapi to the foundation stack

--- terraform/foundation/network.tf
+++ terraform/foundation/network.tf
@@ -8,3 +8,23 @@ data "aws_subnets" "main" {
     values = [data.aws_vpc.main.id]
   }
 }
+
+resource "aws_security_group" "fastapi" {
+  name        = "fraud-api-ecs-sg"
+  description = "security group for fraud-detection ecs"
+  vpc_id      = data.aws_vpc.main.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "fastapi" {
+  security_group_id            = aws_security_group.fastapi.id
+  referenced_security_group_id = "sg-066f821d15bebc3e8"
+  from_port                    = 8000
+  to_port                      = 8000
+  ip_protocol                  = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "fastapi" {
+  security_group_id = aws_security_group.fastapi.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
