da08bb3c988d5252365f266a67456ec033b83595
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-23 21:27:35 +0200

    Added data

--- terraform/application/network.tf
+++ terraform/application/network.tf
@@ -9,30 +9,12 @@ data "aws_subnets" "main" {
   }
 }
 
-resource "aws_security_group" "alb" {
-  name        = "fraud-api-alb-sg"
-  description = "security group for fraud-detection alb"
-  vpc_id      = data.aws_vpc.main.id
+data "aws_security_group" "fastapi" {
+  id = "sg-08ba2c3695a8a909f"
 }
 
-resource "aws_security_group" "fastapi" {
-  name        = "fraud-api-ecs-sg"
-  description = "security group for fraud-detection ecs"
-  vpc_id      = data.aws_vpc.main.id
-}
-
-resource "aws_vpc_security_group_ingress_rule" "fastapi" {
-  security_group_id            = aws_security_group.fastapi.id
-  referenced_security_group_id = "sg-066f821d15bebc3e8"
-  from_port                    = 8000
-  to_port                      = 8000
-  ip_protocol                  = "tcp"
-}
-
-resource "aws_vpc_security_group_egress_rule" "fastapi" {
-  security_group_id = aws_security_group.fastapi.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
+data "aws_security_group" "alb" {
+  name = "sg-08e444fef6fe92788"
 }
 
 resource "aws_security_group" "streamlit" {
@@ -46,7 +28,7 @@ resource "aws_vpc_security_group_ingress_rule" "streamlit" {
   to_port                      = 8501
   from_port                    = 8501
   ip_protocol                  = "tcp"
-  referenced_security_group_id = "sg-066f821d15bebc3e8"
+  referenced_security_group_id = data.aws_security_group.alb.id
 }
 
 resource "aws_vpc_security_group_egress_rule" "streamlit" {
