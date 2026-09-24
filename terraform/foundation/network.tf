eec05f0ac7aec69a5b310854120a1c6786d5b306
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-24 11:33:49 +0200

    Applied terraform fmt

--- terraform/foundation/network.tf
+++ terraform/foundation/network.tf
@@ -17,18 +17,18 @@ resource "aws_security_group" "alb" {
 
 resource "aws_vpc_security_group_ingress_rule" "alb_fastapi" {
   security_group_id = aws_security_group.alb.id
-  cidr_ipv4 = "0.0.0.0/0"
-  ip_protocol = "tcp"
-  from_port = 80
-  to_port = 80
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 80
+  to_port           = 80
 }
 
 resource "aws_vpc_security_group_ingress_rule" "alb_streamlit" {
   security_group_id = aws_security_group.alb.id
-  cidr_ipv4 = "0.0.0.0/0"
-  ip_protocol = "tcp"
-  from_port = 8501
-  to_port = 8501
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+  from_port         = 8501
+  to_port           = 8501
 }
 resource "aws_vpc_security_group_egress_rule" "alb" {
   security_group_id = aws_security_group.alb.id
