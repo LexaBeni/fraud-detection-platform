6c6d7127f3158e5ebd1d8346a2f5afe53b3a1bea
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-09-24 11:29:49 +0200

    Fixed alb security group

--- terraform/foundation/network.tf
+++ terraform/foundation/network.tf
@@ -15,6 +15,27 @@ resource "aws_security_group" "alb" {
   vpc_id      = data.aws_vpc.main.id
 }
 
+resource "aws_vpc_security_group_ingress_rule" "alb_fastapi" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port = 80
+  to_port = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_streamlit" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port = 8501
+  to_port = 8501
+}
+resource "aws_vpc_security_group_egress_rule" "alb" {
+  security_group_id = aws_security_group.alb.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
 resource "aws_security_group" "fastapi" {
   name        = "fraud-api-ecs-sg"
   description = "security group for fraud-detection ecs"
